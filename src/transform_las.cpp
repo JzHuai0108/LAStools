9fcb6094f431da3f4994158fc094e8e2b9608268
jhuai <huai.3@osu.edu>
2023-10-22 10:51:38 +0800

    transform and save las file given a 4x4 matrix

--- src/transform_las.cpp
+++ src/transform_las.cpp
@@ -35,6 +35,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
 #include "lasreader.hpp"
 #include "laswriter.hpp"
@@ -42,9 +46,9 @@
 void usage(bool wait=false)
 {
   fprintf(stderr,"usage:\n");
-  fprintf(stderr,"lasexample in.las out.las\n");
-  fprintf(stderr,"lasexample -i in.las -o out.las -verbose\n");
-  fprintf(stderr,"lasexample -ilas -olas < in.las > out.las\n");
+  fprintf(stderr,"lasexample -t trans.txt in.las out.las\n");
+  fprintf(stderr,"lasexample -t trans.txt -i in.las -o out.las -verbose\n");
+  fprintf(stderr,"lasexample -t trans.txt -ilas -olas < in.las > out.las\n");
   fprintf(stderr,"lasexample -h\n");
   if (wait)
   {
@@ -69,12 +73,65 @@ static double taketime()
   return (double)(clock())/CLOCKS_PER_SEC;
 }
 
+bool load_transform(const std::string &filename, std::vector<std::vector<double>> &total_data) {
+  // read four lines in the trans file
+  // each line has 4 numbers
+  // the last line is 0, 0, 0, 1
+  // the first three lines are the transform matrix
+  // adapted from Fast-Robust-ICP/io_pc.h
+	std::ifstream input(filename);
+	std::string line;
+	int rows, cols;
+	while (getline(input, line)) {
+        if(line[0] == 'V' || line[0] == 'M')
+            continue;
+		std::istringstream iss(line);
+		std::vector<double> lineVec;
+		while (iss) {
+			double item;
+			if (iss >> item)
+				lineVec.push_back(item);
+		}
+		cols = lineVec.size();
+		total_data.push_back(lineVec);
+	}
+	if (total_data.size() == 0)
+	{
+		std::cout << filename << " is empty !! " << std::endl;
+		return false;
+	}
+	rows = total_data.size();
+  input.close();
+  std::cout << "read trans = " << std::endl;
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++)
+      std::cout << total_data[i][j] << " ";
+    std::cout << std::endl;
+  }
+	return true;
+}
+
+void transform_point(const std::vector<std::vector<double>> &T, 
+        const std::vector<double> &point,
+        std::vector<double> &outpoint) {
+  // point is a 3d point
+  // T is a 4x4 matrix
+  // outpoint is a 3d point
+  for (int i = 0; i < 3; i++) {
+    outpoint[i] = 0;
+    for (int j = 0; j < 3; j++) {
+      outpoint[i] += T[i][j] * point[j];
+    }
+    outpoint[i] += T[i][3];
+  }
+}
+
 int main(int argc, char *argv[])
 {
   int i;
   bool verbose = false;
   double start_time = 0.0;
-
+  std::string trans_filename;
   LASreadOpener lasreadopener;
   LASwriteOpener laswriteopener;
 
@@ -109,6 +166,9 @@ int main(int argc, char *argv[])
     {
       verbose = true;
     }
+    else if (strcmp(argv[i],"-t") == 0) {
+      trans_filename = argv[++i];
+    }
     else if (i == argc - 2 && !lasreadopener.active() && !laswriteopener.active())
     {
       lasreadopener.set_file_name(argv[i]);
@@ -131,7 +191,10 @@ int main(int argc, char *argv[])
   if (verbose) start_time = taketime();
 
   // check input & output
-
+  if (trans_filename.empty()) {
+    fprintf(stderr, "ERROR: You should provide transformation.txt like '-t trans.txt'\n");
+    usage(argc == 1);
+  }
   if (!lasreadopener.active())
   {
     fprintf(stderr,"ERROR: no input specified\n");
@@ -168,22 +231,30 @@ int main(int argc, char *argv[])
   if (verbose) fprintf(stderr, "reading %lld points from '%s' and writing them modified to '%s'.\n", lasreader->npoints, lasreadopener.get_file_name(), laswriteopener.get_file_name());
 #endif
 
+  std::vector<std::vector<double>> T;
+  load_transform(trans_filename, T);
   // loop over points and modify them
   int count = 0;
   // where there is a point to read
   while (lasreader->read_point())
   {
     // modify the point
-    lasreader->point.set_point_source_ID(1020);
-    lasreader->point.set_user_data(42);
-    if (lasreader->point.get_classification() == 12) lasreader->point.set_classification(1);
-    // lasreader->point.set_Z(lasreader->point.get_Z() + 10);
+    std::vector<double> point(3, 0);
+    point[0] = lasreader->point.get_x();
+    point[1] = lasreader->point.get_y();
+    point[2] = lasreader->point.get_z();
+    std::vector<double> outpoint(3, 0);
+    transform_point(T, point, outpoint);
+    lasreader->point.set_x(outpoint[0]); // note the changes will not be saved into the file of the reader.
+    lasreader->point.set_y(outpoint[1]);
+    lasreader->point.set_z(outpoint[2]);
+
     // write the modified point
     laswriter->write_point(&lasreader->point);
     // add it to the inventory
     laswriter->update_inventory(&lasreader->point);
     if (count < 5) {
-      printf("%d: X %d Y %d Z %d x %.6f y %.6f z %.6f R %u G %u B %u z scale %.6f z offset %.6f\n", 
+      printf("After: %d: X %d Y %d Z %d x %.6f y %.6f z %.6f R %u G %u B %u z scale %.6f z offset %.6f\n", 
           count, lasreader->point.get_X(), lasreader->point.get_Y(), lasreader->point.get_Z(),
           lasreader->point.get_x(), lasreader->point.get_y(), lasreader->point.get_z(), 
           lasreader->point.get_R(), lasreader->point.get_G(), lasreader->point.get_B(), 
